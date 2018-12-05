4c8edba43520e0df406dc810b289f67a34fe6efc
Michael Nolan <michaelnolan@Michaels-MacBook-Air.local>
2018-12-05 22:05:44 +1000

    rock paper scissor v93824766

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -24,13 +24,13 @@ function getComputerChoice() {
 function getUserImage() {
 	switch (userInput) {
 		case 'rock':
-			$('.image1').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwigoOGfjojfAhWIA3IKHQm6BK0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.kisspng.com%2Fpng-graphic-design-rock-vector-1272079%2Fpreview.html&psig=AOvVaw2wKTLemg9-SwUfRkUZxpfC&ust=1544079424151875');
+			$('.image1').attr('src', 'https://png2.kisspng.com/sh/ac0fbc33b6eedebca8a9be544d8c9c4a/L0KzQYm3VME6N6lrfZH0aYP2gLBuTfdzaaFtgdU2ZHX2ebj1TgJwa5wyjtdsdHB1PYbohMg0bZRme6oBMHO3PoK8V8AxO2o2Sac7NEG1QYOCUMgzOGkziNDw/kisspng-graphic-design-rock-vector-5ad83ecac860c4.1570039115241212908208.png');
 			break;	
 		case 'paper':
-			$('.image1').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi_noSHk4jfAhXKEHIKHUhIAc8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.clipartpanda.com%2Fcategories%2Fstack-of-paper-vector&psig=AOvVaw0cXOH2CgC8IxdHLwUZ_5DP&ust=1544080703418239')
+			$('.image1').attr('src', 'http://images.clipartpanda.com/stack-of-paper-vector-29753.png')
 			break;
 		case 'scissors':
-			$('.image1').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjApfaSk4jfAhVVVH0KHdAxDToQjRx6BAgBEAU&url=http%3A%2F%2Fworldartsme.com%2Fscissors-vector-clipart.html&psig=AOvVaw0m6T3fHj9fDD0SYTFU17of&ust=1544080738476275')
+			$('.image1').attr('src', 'http://worldartsme.com/images/scissors-vector-clipart-1.jpg')
 			break;
 	}
 }
@@ -38,13 +38,13 @@ function getUserImage() {
 function getComputerImage() {
 	switch (userInput) {
 		case 'rock':
-			$('.image2').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwigoOGfjojfAhWIA3IKHQm6BK0QjRx6BAgBEAU&url=https%3A%2F%2Fwww.kisspng.com%2Fpng-graphic-design-rock-vector-1272079%2Fpreview.html&psig=AOvVaw2wKTLemg9-SwUfRkUZxpfC&ust=1544079424151875');
+			$('.image2').attr('src', 'https://png2.kisspng.com/sh/ac0fbc33b6eedebca8a9be544d8c9c4a/L0KzQYm3VME6N6lrfZH0aYP2gLBuTfdzaaFtgdU2ZHX2ebj1TgJwa5wyjtdsdHB1PYbohMg0bZRme6oBMHO3PoK8V8AxO2o2Sac7NEG1QYOCUMgzOGkziNDw/kisspng-graphic-design-rock-vector-5ad83ecac860c4.1570039115241212908208.png');
 			break;	
 		case 'paper':
-			$('.image2').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi_noSHk4jfAhXKEHIKHUhIAc8QjRx6BAgBEAU&url=http%3A%2F%2Fwww.clipartpanda.com%2Fcategories%2Fstack-of-paper-vector&psig=AOvVaw0cXOH2CgC8IxdHLwUZ_5DP&ust=1544080703418239')
+			$('.image2').attr('src', 'http://images.clipartpanda.com/stack-of-paper-vector-29753.png')
 			break;
 		case 'scissors':
-			$('.image2').attr('src', 'https://www.google.com.au/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjApfaSk4jfAhVVVH0KHdAxDToQjRx6BAgBEAU&url=http%3A%2F%2Fworldartsme.com%2Fscissors-vector-clipart.html&psig=AOvVaw0m6T3fHj9fDD0SYTFU17of&ust=1544080738476275')
+			$('.image2').attr('src', 'http://worldartsme.com/images/scissors-vector-clipart-1.jpg')
 			break;
 	}
 }
@@ -116,6 +116,8 @@ $('document').ready(() => {
 	$('.userInput').on('click' , () => {
 		getOutcome();
 		showWinner();
+		getComputerImage();
+		getUserImage();
 	});
 
 	$('.restart').on('click' , () => {
